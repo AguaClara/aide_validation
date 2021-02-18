db7fea343dc71c8fe146cdcbb03e04a55bb3d89d
Fletcher Chapin <fletchapin@gmail.com>
2021-02-17 17:39:03 -0800

    Bump version: 0.0.0 → 0.0.1

--- setup.py
+++ setup.py
@@ -82,6 +82,6 @@ setup(
     url="https://github.com/AguaClara/aide_validation",
     # Do not edit this string manually, always use bumpversion
     # Details in CONTRIBUTING.rst
-    version="0.0.0",
+    version="0.0.1",
     zip_safe=False,
 )
